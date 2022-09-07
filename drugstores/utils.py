e113e98caaa7c735b7c73dab450f049d551134f1
Vladimir_Kirichenko <frozenfish@internet.ru>
2022-09-08 00:37:08 +0300

    - работа над schedule_representation, работа с представление расписания в одну строку вынесена в функцию get_schedule_representation - в данной функции описано условие работы - круглосуточно - описано условие работы - ежедневно - описано условие работы при разных режимах работы в разные дни (если время одно у трех и более дней, но дни не стоят рядом, то формируется неправльно)

--- drugstores/utils.py
+++ drugstores/utils.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def get_schedule_representation(data):
     open_list = [
         data.schedule.monday_open,
@@ -36,6 +38,25 @@ def get_schedule_representation(data):
             len(open_set) == 1
             and len(close_set) == 1
     ):
-        return f'ежедневно {open_set.pop().strftime("%H:%M")}-{close_set.pop().strftime("%H:%M")}'
+        return (f'ежедневно '
+                f'{open_set.pop().strftime("%H:%M")}'
+                f'-{close_set.pop().strftime("%H:%M")}')
 
-    return 'Не удалось объединить расписание в одну строку'
+    """формирование расписания при разных режимах работы"""
+    schedule_dict = defaultdict(list)
+    schedule_dict[f'{data.schedule.monday_open.strftime("%H:%M")}-{data.schedule.monday_close.strftime("%H:%M")}'].append('пн')
+    schedule_dict[f'{data.schedule.tuesday_open.strftime("%H:%M")}-{data.schedule.tuesday_close.strftime("%H:%M")}'].append('вт')
+    schedule_dict[f'{data.schedule.wednesday_open.strftime("%H:%M")}-{data.schedule.wednesday_close.strftime("%H:%M")}'].append('ср')
+    schedule_dict[f'{data.schedule.thursday_open.strftime("%H:%M")}-{data.schedule.thursday_close.strftime("%H:%M")}'].append('чт')
+    schedule_dict[f'{data.schedule.friday_open.strftime("%H:%M")}-{data.schedule.friday_close.strftime("%H:%M")}'].append('пт')
+    schedule_dict[f'{data.schedule.saturday_open.strftime("%H:%M")}-{data.schedule.saturday_close.strftime("%H:%M")}'].append('сб')
+    schedule_dict[f'{data.schedule.sunday_open.strftime("%H:%M")}-{data.schedule.sunday_close.strftime("%H:%M")}'].append('вс')
+
+    schedule_str = ''
+    for key, value in schedule_dict.items():
+        if len(value) > 2:
+            schedule_str += f'{value[0]}-{value[-1]} {key} '
+        else:
+            schedule_str += f'{",".join(value)} {key} '
+
+    return schedule_str.rstrip()
